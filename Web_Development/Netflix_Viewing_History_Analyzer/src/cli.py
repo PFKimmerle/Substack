86fd86e308209acffa886fc6aa9033dc080d56cd
pfkimmerle <pfkimmerle@gmail.com>
2024-09-16 18:09:32 -0700

    update cli.py

--- Web_Development/Netflix_Viewing_History_Analyzer/src/cli.py
+++ Web_Development/Netflix_Viewing_History_Analyzer/src/cli.py
@@ -3,11 +3,17 @@ from file_handler import load_csv
 from analysis import analyze_viewing_history
 
 @click.command()
-@click.option('--file', prompt='Netflix History CSV File', help='Path to Netflix viewing history CSV file')
-def main(file):
+@click.option('--file', '-f', required=True, type=click.Path(), help='Specify the path to your Netflix viewing history CSV file.')
+def main(file: str):
     """Analyzes your Netflix viewing habits."""
-    data = load_csv(file)
-    analyze_viewing_history(data)
+    try:
+        data = load_csv(file)
+        analyze_viewing_history(data)
+        print("Results have been successfully analyzed.")
+    except FileNotFoundError:
+        print(f"Error: The file '{file}' was not found. Please check the file path and try again.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == '__main__':
     main()
