c61a0621c824108eff6b654b804cdd1dbf29aa3b
pfkimmerle <pfkimmerle@gmail.com>
2024-10-21 14:26:25 -0700

    updated

--- Development_Workflows/CICD_Pipeline/tests/test_appy.py
+++ Development_Workflows/CICD_Pipeline/tests/test_appy.py
@@ -1,12 +1,12 @@
 import pytest
-import sys
-import os
 from app import greet
 
 @pytest.mark.parametrize(
-    "name,expected",
-    [("Intern", "Welcome to our CI/CD project!"),
-    ("Developer", "Welcome to our CI/CD project!")]
+    "name,expected", [
+        ("Intern", "Hello, Intern!"),
+        ("Developer", "Hello, Developer!")
+    ]
 )
+
 def test_greet(name, expected):
     assert greet(name) == expected
