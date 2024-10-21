e0a87e3ac87d1075d8304090b233d9df268d2fe3
pfkimmerle <pfkimmerle@gmail.com>
2024-10-21 14:21:24 -0700

    updated

--- Development_Workflows/CICD_Pipeline/app.py
+++ Development_Workflows/CICD_Pipeline/app.py
@@ -1,8 +1,8 @@
 def greet(name):
-    """Greet a person by name."""
+    """
+    Greet a person by name.
+    """
     return f"Hello, {name}! Welcome to our CI/CD project!"
-
 
 if __name__ == "__main__":
     print(greet("Intern"))
-    
--- Development_Workflows/CICD_Pipeline/tests/test_appy.py
+++ Development_Workflows/CICD_Pipeline/tests/test_appy.py
@@ -1,13 +1,12 @@
 import pytest
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import greet
 
-
-@pytest.mark.parametrize("name,expected", [
-    ("Intern", "Hello, Intern! Welcome to our CI/CD project!"),
-    ("Developer", "Hello, Developer!")
-])
+@pytest.mark.parametrize(
+    "name,expected",
+    [("Intern", "Welcome to our CI/CD project!"),
+    ("Developer", "Welcome to our CI/CD project!")]
+)
 def test_greet(name, expected):
     assert greet(name) == expected
